31b1a61bbf8bbe44494f19826216f5217e5ca2f8
SN78196 <SN78196@125101-14125>
2019-09-25 18:59:35 +0800

    201909251123001

---
+++ MSXslCompiledTransformDemo/MSXslCompiler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+
+namespace MSXslCompiler
+{
+    class Program
+    {
+        private const string sourceFile = @"./Resources/books.xml";
+        private const string stylesheet = @"./Resources/output.xsl";
+        private const string outputFile = @"./Resources/output.html";
+
+        static void Main(string[] args)
+        {
+            
+            // Enable XSLT debugging.  
+            XslCompiledTransform xslt = new XslCompiledTransform(true);
+            
+            XsltSettings settings = new XsltSettings();
+            settings.EnableScript = true;
+
+            //XmlSecureResolver resolver = new XmlSecureResolver();
+
+            // Compile the style sheet.  
+            xslt.Load(stylesheet,settings,null) ;
+
+            // Execute the XSLT transform.  
+            //FileStream outputStream = new FileStream(outputFile, FileMode.Append);
+            using (var outputStream = new FileStream(outputFile,FileMode.Append))
+            {
+                xslt.Transform(sourceFile, null, outputStream);
+            }
+              
+           
+        }
+    }
+}
